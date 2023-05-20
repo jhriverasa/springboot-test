125be7285102964e819b2c5d9a8f578ccf16e0cc
jhriverasa <jhriverasa@unal.edu.co>
2023-05-20 17:20:37 -0500

    initial commit

---
+++ src/main/java/com/springapitest/restapi/models/User.java
@@ -0,0 +1,43 @@
+package com.springapitest.restapi.models;
+
+public class User {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    private String lastName;
+    private String phone;
+
+}
